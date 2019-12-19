e1d923a8130c355cc1d3645fe84d3fe1cfa143b7
cruzador <batyrqh@protonmail.com>
2019-12-19 16:34:02 +0300

    added card schema

--- models/card.js
+++ models/card.js
@@ -3,15 +3,7 @@ const mongoose = require('mongoose');
 const cardSchema = new mongoose.Schema({
   day: Number,
   month: String,
-  time1: Number,
-  time2: Number,
-  time3: Number,
-  time4: Number,
-  time5: Number,
-  time6: Number,
-  time7: Number,
-  time8: Number,
-  time9: Number,
+  time: [{ type: Number }],
 });
 
 module.exports = mongoose.model('Card', cardSchema);
---
+++ seeder/cardSeeder.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(
+  'mongodb+srv://tenderUser:b.-6EEHk!7obdBxTshEP@cluster0-kuoil.mongodb.net/test?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+);
+const Test = require('../models/test');
+
+const test1 = new Test({
+  question: 'Which one is not a variable type in JS?',
+  options: ['undefined', 'null', 'NaN', 'function'],
+  answer: 'NaN',
+});
+const test2 = new Test({
+  question: 'What is the loo?',
+  options: ['undefined', 'null', 'NaN', 'function'],
+  answer: 'Iteration',
+});
+const test3 = new Test({
+  question: 'How is life?',
+  options: ['undefined', 'null', 'NaN', 'function'],
+  answer: 'All good',
+});
+const test4 = new Test({
+  question: 'You good?',
+  options: ['undefined', 'null', 'NaN', 'function'],
+  answer: 'NaN',
+});
+const test5 = new Test({
+  question: 'Which one is not a variable type in JS?',
+  options: ['undefined', 'null', 'NaN', 'function'],
+  answer: 'NaN',
+});
+
+test1.save();
+test2.save();
+test3.save();
+test4.save();
+test5.save();
