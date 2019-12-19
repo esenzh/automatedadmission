afb30440f6f76bda51222d0e276d897a2aea64a9
cruzador <batyrqh@protonmail.com>
2019-12-19 16:16:39 +0300

    fixed by eslint

--- routes/user.js
+++ routes/user.js
@@ -1,20 +1,25 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+
+const router = express.Router();
 const Test = require('../models/test');
 
 /* Get to test page. */
 router.get('/test', async (req, res, next) => {
-    const test = await Test.find({});
-    res.render('test', {test});
+  const test = await Test.find({});
+  res.render('test', { test });
 });
 
 router.get('/start-test', async (req, res) => {
-    if (!req.session.user) {
-      const message = 'Please Login before starting test or Register';
-      res.redirect(`/login?message=${message}`);
-    } else {
-        res.redirect('/user/test');
-    }
+  if (!req.session.user) {
+    const message = 'Please Login before starting test or Register';
+    res.redirect(`/login?message=${message}`);
+  } else {
+    res.redirect('/user/test');
+  }
+});
+
+router.get('/calendar', (req, res) => {
+  res.render('calendar');
 });
 
 module.exports = router;
